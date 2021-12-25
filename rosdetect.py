b4da10c162869484f93b8393a23cb1d87b5d71bc
St333fan <96583923+St333fan@users.noreply.github.com>
2021-12-25 21:06:00 +0100

    Create rosdetect.py

---
+++ rosdetect.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python
+
+# YOLOv5 ðŸš€ by Ultralytics, GPL-3.0 license
+"""
+Run inference on images, videos, directories, streams, etc.
+
+Usage:
+    $ python path/to/detect.py --weights yolov5s.pt --source 0  # webcam
+                                                             img.jpg  # image
+                                                             vid.mp4  # video
+                                                             path/  # directory
+                                                             path/*.jpg  # glob
+                                                             'https://youtu.be/Zgi9g1ksQHc'  # YouTube
+                                                             'rtsp://example.com/media.mp4'  # RTSP, RTMP, HTTP st>
+"""
+# rospy for the subscriber
+import rospy
+import sys
+# ROS Image message
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+# ROS Image message -> OpenCV2 image converter
+from cv_bridge import CvBridge, CvBridgeError
+
+from utils.augmentations import letterbox#fÃ¼r im0 to im in dataset
+import time
+import numpy as np
+
+import argparse
+import os
+import sys
+from pathlib import Path
+
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # YOLOv5 root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+
+from models.experimental import attempt_load
+from models.common import DetectMultiBackend
+from utils.datasets import IMG_FORMATS, VID_FORMATS, LoadImages, LoadStreams
+from utils.general import (LOGGER, check_file, check_img_size, check_imshow, check_requirements, colorstr,
+                           increment_path, set_logging, non_max_suppression, print_args, scale_coords, strip_optim>
+from utils.plots import Annotator, colors, save_one_box
+from utils.torch_utils import select_device, time_sync
+
+bridge = CvBridge()
+
+@torch.no_grad()
+class subscriber:
+    
+    def __init__(self):
+
+        weights = ROOT / 'yolov5s.pt'  # model.pt path(s)
+        source = ROOT / 'data/images'  # file/dir/URL/glob, 0 for webcam
+        self.imgsz = (640, 640)  # inference size (height, width)
+        self.conf_thres = 0.25  # confidence threshold
+        self.iou_thres = 0.45 # NMS IOU threshold
+        self.max_det = 1000  # maximum detections per image
+        self.device = ''  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+        self.view_img = False  # show results
+        self.save_txt = False # save results to *.txt
+        self.save_conf = False  # save confidences in --save-txt labels
+        self.save_crop = False  # save cropped prediction boxes
+        nosave = False  # do not save images/videos
+        self.classes = None  # filter by class: --class 0, or --class 0 2 3
+        self.agnostic_nms = False  # class-agnostic NMS
+        self.augment = False  # augmented inference
+        self.visualize = False  # visualize features
+        self.update = False  # update all models
+        project = ROOT / 'runs/detect'  # save results to project/name
+        name = 'exp'  # save results to project/name
+        exist_ok = False  # existing project/name ok, do not increment
+        self. line_thickness = 3  # bounding box thickness (pixels)
+        self.hide_labels = False  # hide labels
+        self.hide_conf = False  # hide confidences
+        self.half = False  # use FP16 half-precision inference
+        dnn = False  # use OpenCV DNN for ONNX inference
+        self.stride = 32
+
+         # Initialize
+        set_logging()
+        self.device = select_device(self.device)
+        self.half &= self.device.type != 'cpu'  # half precision only supported on CUDA
+
+        # Load model
+        self.model = attempt_load(weights, map_location=self.device)  # load FP32 model
+        stride = int(self.model.stride.max())  # model stride
+        imgsz = check_img_size(self.imgsz, s=stride)  # check image size
+        self.names = self.model.module.names if hasattr(self.model, 'module') else self.model.names  # get class n>
+        if self.half:
+            self.model.half()  # to FP16
+
+        # Second-stage classifier
+        self.classify = False
+        if self.classify:
+            self.modelc = load_classifier(name='resnet50', n=2)  # initialize
+            self.modelc.load_state_dict(torch.load('resnet50.pt', map_location=self.device)['model']).to(self.devi>
+
+        # Dataloader
+        #view_img = check_imshow()
+        cudnn.benchmark = True  # set True to speed up constant image size inference
+
+        # Run inference
+        if self.device.type != 'cpu':
+            self.model(torch.zeros(1, 3, imgsz, imgsz).to(self.device).type_as(next(model.parameters())))  # run o>
+
+        self.sub = rospy.Subscriber("/rgb/image_raw", Image, self.callback)
+
+    def callback(self, data):
+        t0 = time.time()
+        img = bridge.imgmsg_to_cv2(data, "bgr8")
+
+        # check for common shapes
+        s = np.stack([letterbox(x, self.imgsz, stride=self.stride)[0].shape for x in img], 0)  # shapes
+        rect = np.unique(s, axis=0).shape[0] == 1  # rect inference if all shapes equal
+        if not rect:
+            print('WARNING: Different stream shapes detected. For optimal performance supply similarly-shaped stre>
+
+        # Letterbox
+        img0 = img.copy()
+        img = letterbox(img0, self.imgsz, stride=self.stride, auto=True)[0]
+
+        img = img[np.newaxis, :, :, :]
+
+        # Stack
+        img = np.stack(img, 0)
+
+        # Convert
+        img = img[..., ::-1].transpose((0, 3, 1, 2))  # BGR to RGB, BHWC to BCHW
+        img = np.ascontiguousarray(img)
+
+        img = torch.from_numpy(img).to(self.device)
+        img = img.half() if self.half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+
+        print("Shape:", img.shape)
+        # Inference
+        t1 = time_sync()
+        pred = self.model(img,
+                          augment=self.augment,
+                          visualize=increment_path( ROOT / 'data/images' / 'features', mkdir=True) if self.visuali>
+
+        # Apply NMS
+        pred = non_max_suppression(pred, self.conf_thres, self.iou_thres, self.classes, self.agnostic_nms,
+                                   max_det=self.max_det)
+        t2 = time_sync()
+
+        # Apply Classifier
+        if self.classify:
+            pred = apply_classifier(pred, self.modelc, img, img0)
+
+        # Process detections
+        for i, det in enumerate(pred):  # detections per image
+            s = f'{i}: '
+            s += '%gx%g ' % img.shape[2:]  # print string
+
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], img0.shape).round()
+
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += f"{n} {self.names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                for *xyxy, conf, cls in reversed(det):
+                    c = int(cls)  # integer class
+                    label = None if self.hide_labels else (
+                        self.names[c] if self.hide_conf else f'{self.names[c]} {conf:.2f}')
+                    #plot_one_box(xyxy, img0, label=label, color=colors(c, True), line_thickness=self.line_thickne>
+
+
+def main():
+    check_requirements(exclude=('tensorboard', 'thop'))
+    obc = subscriber()
+    rospy.init_node('yolo', anonymous=True)
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:    
+        print("Shutting down")
+
+if __name__ == "__main__":
+    main()
